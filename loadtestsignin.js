86292fc95db6aacc50abf09173206970f2b1d394
Andrew Kiema <andrewkiema94@gmail.com>
2025-05-21 12:48:39 +0300

    Add load test for sign in page

--- loadtestsignin.js
+++ loadtestsignin.js
@@ -1,1 +1,21 @@
 import http from 'k6/http';
+
+export const options={
+    stages:[
+        {
+          duration:'10s', 
+          target:20  
+        },
+        {
+            duration:'40s',
+            target:20
+        },
+        {
+            duration:'10s',
+            target:0
+        }
+    ]
+}
+export default function(){
+    http.get('https://resolve.vote/signin');
+}
