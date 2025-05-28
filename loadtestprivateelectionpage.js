e421d8ba48684db17e7593ed9ee39749bde7a0bf
Andrew Kiema <andrewkiema94@gmail.com>
2025-05-28 10:28:02 +0300

    Perform load test on private election page

---
+++ loadtestprivateelectionpage.js
@@ -0,0 +1,22 @@
+import http from'k6/http';
+
+export const options={
+    stages:[
+        {
+            duration:'10s',
+            target:20
+        },
+        {
+            duration:'4m',
+            target:20
+        },
+        {
+            duration:'20s',
+            target:0
+        }
+    ]
+}
+
+export default function(){
+    http.get('https://resolve.vote/elections/create?mode=private');
+}
