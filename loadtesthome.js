b1ca2161f1d43a2d3b9e5db4c2f6f576a2f98361
Andrew Kiema <andrewkiema94@gmail.com>
2025-05-28 12:14:31 +0300

    Update explanation

--- loadtesthome.js
+++ loadtesthome.js
@@ -3,11 +3,11 @@ import http from 'k6/http'; //This imports the HTTP module from K6, allowing you
 export const options={ //This object defines the stages of the load test
     stages:[ //stages: An array of objects that defines the load pattern over time.
         {
-            duration:'10s', //For the first 10 seconds, 50 VUs will be active.
+            duration:'10s', //For the first 10 seconds, 20 VUs will be active.
             target: 20
         },
         {
-            duration: '20s', //For the next 20 seconds, 50 VUs will continue to be active
+            duration: '20s', //For the next 20 seconds, 20 VUs will continue to be active
             target: 20
         },
         {
