e10c9aad71a946a86b9bcdd72c57a9e964379a1e
Andrew Kiema <andrewkiema94@gmail.com>
2025-06-11 10:48:59 +0300

    Perform spike test on election page

---
+++ spiketestelectionspage.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+export const options={
+    stages:[
+        {
+            target:250,
+            duration:"1m"
+        },
+        {
+            target:1000,
+            duration:"30s"
+        },
+        {
+            target:0,
+            duration:"20s"
+        }
+    ]
+}
+export default function(){
+    http.get("https://resolve.vote/elections");
+}
